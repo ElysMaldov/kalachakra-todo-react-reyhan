6045beef3b35345c5b8041f9788945618e1ce3c8
Elys Maldov <maldovelys@gmail.com>
2025-08-04 22:32:00 +0700

    feat: setup React Query

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,17 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router";
 import "./styles/globals.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query/query-client.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>
 );
