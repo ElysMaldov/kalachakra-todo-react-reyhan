5170f5f212b14f0765bdfcc37d31bb1a7e486e26
Elys Maldov <maldovelys@gmail.com>
2025-08-05 01:10:46 +0700

    feat: create Input

---
+++ src/components/inputs/Input/input.tsx
@@ -0,0 +1,30 @@
+import { mergeClass } from "@/lib/merge-class";
+import type { ComponentProps, ReactNode } from "react";
+
+export interface InputProps extends ComponentProps<"input"> {
+  trailingIcon?: ReactNode;
+}
+
+const Input = ({ className, trailingIcon, ...props }: InputProps) => {
+  // TODO add dark mode
+  return (
+    <div className={mergeClass("relative w-full")}>
+      <input
+        type="text"
+        {...props}
+        className={mergeClass(
+          "w-full",
+          "border-accent rounded-[5px] border px-4 py-2",
+          "font-inter font-medium",
+          "placeholder:text-accent-ghost",
+          "text-accent text-base",
+          "focus-visible:border-accent focus-visible:ring-accent-ghost focus-visible:outline-none focus-visible:ring-[2px]",
+          className
+        )}
+      />
+      <div className="absolute bottom-2.5 right-4 top-2.5">{trailingIcon}</div>
+    </div>
+  );
+};
+
+export default Input;
